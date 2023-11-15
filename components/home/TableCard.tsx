80f599c2653b8c9058e81285cb615c4f353d3581
jeff0518 <cycivs07@icloud.com>
2023-11-15 16:45:39 +0800

    feat: 首頁製作

---
+++ components/home/TableCard.tsx
@@ -0,0 +1,33 @@
+import ButtonUI from "../shared/ButtonUI";
+
+import style from "./TableCard.module.scss";
+
+interface TableCardProps {
+  tableId: number;
+  totalAmount: number;
+  diningTime: string;
+}
+
+function TableCard(props: TableCardProps) {
+  const { tableId, totalAmount, diningTime } = props;
+  const orderHandler = () => {};
+  const checkoutHandler = () => {};
+  return (
+    <div className={style.tableCard_container}>
+      {/* 桌號 */}
+      <div className={style.title}>{tableId}桌</div>
+      {/* 顯示資訊 */}
+      <div className={style.information}>
+        <div>金額：{totalAmount}</div>
+        <div>用餐時間：{diningTime}</div>
+      </div>
+      {/* 控制面板（點餐、結帳） */}
+      <div className={style.control}>
+        <ButtonUI text="點餐" btnStyle="btn__pill" onClick={orderHandler} />
+        <ButtonUI text="結帳" btnStyle="btn__pill" onClick={checkoutHandler} />
+      </div>
+    </div>
+  );
+}
+
+export default TableCard;
---
+++ pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Home from "@/components/home/Home";
+
+import style from "./index.module.scss";
+function HomePage() {
+  return (
+    <div className={style.div}>
+      <Home />
+    </div>
+  );
+}
+
+export default HomePage;
