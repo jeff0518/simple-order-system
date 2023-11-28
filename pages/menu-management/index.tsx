18105065454fcc5ac5aed0023981943abe3b5490
jeff0518 <cycivs07@icloud.com>
2023-11-28 19:16:13 +0800

    fix: 新增按鈕 的的css優化

--- pages/menu-management/index.tsx
+++ pages/menu-management/index.tsx
@@ -1,9 +1,16 @@
+import { IoMdAdd } from "react-icons/io";
+
 import MenuCard from "@/components/card/MenuCard";
 
 import style from "./index.module.scss";
 function MenuManagement() {
   return (
     <div className={style.menuManagement_container}>
+      <div className={style.addImage}>
+        <button className={style.addImage__btn} onClick={() => {}}>
+          <IoMdAdd size={50} />
+        </button>
+      </div>
       <div className={style.imgBox}>
         <MenuCard />
         <MenuCard />
